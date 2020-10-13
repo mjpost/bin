4d3d4ad1fe4c5512287968402e0b72ccbf2801af
Matt Post <post@cs.jhu.edu>
2020-10-13 11:45:02 -0400

    added template

---
+++ template.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def main(args):
+    pass
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
+    main(args)
